d619f110bb9532d0f47d973469e614d4601d51d4
Ahmed Sameh <ahmedsaameh14@gmail.com>
2025-12-14 02:12:28 +0200

    Add Authorization Middleware

--- routes/Reg.route.js
+++ routes/Reg.route.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 const {createUser , getUser , getUserById , deleteUserById} = require('../controllers/Reg.controller');
 const {authenticate} = require('../middlewares/auth.middleware')
+const {authorize} = require('../middlewares/role.middleware')
 
-router.get('/' , authenticate,getUser);
-router.post('/' ,createUser('user'));
+router.post('/' ,createUser('user'));       // Normal User Registration 
+
 router.post('/createAdmin' , authenticate , createUser('admin'));        // Only Admin can Create New Admins
-router.get('/:id' , authenticate , getUserById);
-router.delete("/:id" , deleteUserById);
+
+router.get('/' , authenticate , authorize('admin') ,getUser);       // Only Admin GET All Users
+
+router.get('/:id' , authenticate , authorize('user') ,  getUserById);            // For each Profile 
+
+router.delete("/:id" , deleteUserById);             // Only for test in Postman
 
 
 module.exports = router;
